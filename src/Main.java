f481f1588043bd3e3925febedc7f367b8b078d1f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-10 10:52:54 +0300

    add sleep method to thread

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 //        System.out.println(Thread.activeCount());
 //        Thread.currentThread().setName("nainnnnnnnn");
 //        System.out.println(Thread.currentThread().getName());
 
+        Thread.currentThread().setPriority(9);
+
         System.out.println(Thread.currentThread().getPriority());
+        System.out.println(Thread.currentThread().isAlive());
+
+        for(int i = 3; i >= 0;i--){
+            System.out.println(i);
+            Thread.sleep(2000);
+        }
 
     }
 }
